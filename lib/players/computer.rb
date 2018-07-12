cc03e69b6040017f1b73c43dc6e752d3db0ec716
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-07-12 04:40:30 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,18 +1,18 @@
 require 'pry'
 module Players
   class Computer < Player
-    attr_accessor :board
-    WIN_COMBINATIONS = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [6, 4, 2]
-    ]
-    #CORNERS = [0, 2, 8, 6]
+#    attr_accessor :board
+#    WIN_COMBINATIONS = [
+#      [0, 1, 2],
+#      [3, 4, 5],
+#      [6, 7, 8],
+#      [0, 3, 6],
+#      [1, 4, 7],
+#      [2, 5, 8],
+#      [0, 4, 8],
+#      [6, 4, 2]
+#    ]
+#    CORNERS = [0, 2, 8, 6]
 
     #winning move?
     #a. iterate over winning combos and see if any of them have 2 cells that equal current's player token and one valid move?
